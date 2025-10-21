96660fc577849cc370fea5f6ba684c62d9c2ae24
Hernan Chamut <hchamut@gmail.com>
2025-10-21 23:07:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1927510280-14c43",
+  "appId": "1:488528825817:web:9230fa7f4e7c9268c21dc8",
+  "apiKey": "AIzaSyA1NZgkFLmu7s_3xawp5093hVTin-SF7_w",
+  "authDomain": "studio-1927510280-14c43.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "488528825817"
+};
